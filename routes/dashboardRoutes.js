f4715e472fe3fb67376a0b2cf8cb8ecc60856093
officialmembypoudel <officialmembypoudel@gmail.com>
2025-07-29 15:19:40 +0545

    -- premissions fixes

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { getDashboardData } from "../controllers/dashboardControllers.js";
+import { checkStaffLevelPermissions } from "../middleware/CheckPermissions.js";
+
+import { checkAuthorization } from "../middleware/checkAuthorization.js";
 
 const router = express.Router();
 
-router.route("/").get(getDashboardData);
+router
+  .route("/")
+  .get(checkAuthorization, checkStaffLevelPermissions, getDashboardData);
 
 export default router;
